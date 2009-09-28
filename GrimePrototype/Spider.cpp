7538852cb426292cec27b535b24f9063e488dcef
eternal <eternal@feistystudios.com>
2009-09-29 00:21:20 +1000

    removed LIBCMT warning

--- GrimePrototype/Spider.cpp
+++ GrimePrototype/Spider.cpp
@@ -2,7 +2,6 @@
 
 Spider::Spider( scene::ISceneManager* sceneManager, irrklang::ISoundEngine* soundEngine, IAnimatedMesh* mesh, IPhysxManager* manager, core::array<Enemy*>* objectArray, Player* player, vector3df position /*= vector3df(-501.0f,100.0f,-230.0f)*/ )
 {
-
     //    Spider
     //    Dead -  10
     //    Standing - 1
